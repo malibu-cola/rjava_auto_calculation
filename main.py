09b2541375a3c54dcd24f8c1bbbb98c4e3cc09f2
Hg <86350771+malibu-cola@users.noreply.github.com>
2021-10-05 10:11:00 +0900

    commit

--- main.py
+++ main.py
@@ -19,22 +19,29 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm, Normalize
 from matplotlib.ticker import MaxNLocator
 
+
+# 初期化の仕方
+# r-javaをデフォルトの状態、デフォルトのディレクトリで開いておく
+# またテキストファイルも同様にデフォルトで開いてい置く
+# DataモジュールのdisplayをGeneralDataだけにしておく
+
 update = "500"
 PolyIndex = "1"
 P0 = "1e-4"
 Vexp = "1e4"
 R0 = "2"
 
-vYe0_0 = ["09", "10", "13","16","19"\
-        "20","23","26","29", "30", "01","03","06"]# 12こ
+vYe0_0 = ["19",\
+        "20","23","26","29", "30", "01","03","06","16"]# 12こ
+        #["09","10", "13"] 終了したYe
 
-vT0_9 = ["7e11", "4e9", "7e9",\
-     "1e10", "4e10", "7e10",\
-         "1e11", "4e11",  "1e12"] # 10こ
+vT0_9 = ["7e10", "1e11", "4e11",  "1e12",\
+     "1e10", "4e10",\
+         "7e11", "4e9", "7e9"] # 10こ
 
-vrho0_10 = ["4e13","1e10","4e10", "7e10",\
+vrho0_10 = ["1e12", "4e12", "7e12",\
     "1e11", "4e11", "7e11",\
-        "1e12", "4e12", "7e12",\
+            "4e13","1e10","4e10", "7e10",\
             "1e13"] # 11こ
 
 cnt = 0
@@ -69,7 +76,7 @@ for Ye0 in vYe0_0:
 
             # screenshotを撮り、保存
             screenshot = pyautogui.screenshot()
-            path_pic_NSE = "../進捗/0916to0924/sample/pic/NSE_"+ filename + ".png"
+            path_pic_NSE = "../進捗/1004to1011/pic/NSE_"+ filename + ".png"
             screenshot.save(path_pic_NSE)
             
             # Dataモジュールを開き、NSEでの分布をtxtdataとして保存
@@ -78,7 +85,7 @@ for Ye0 in vYe0_0:
             rjava.DtSv(txtdata_NSE)
 
             # テキストデータをこのpythonで開く
-            path_open_NSE = "../進捗/0916to0924/sample/data/" + txtdata_NSE
+            path_open_NSE = "../進捗/1004to1011/data/" + txtdata_NSE
             with open(path_open_NSE) as f:
                 line = f.readlines()
             print(line[1]) # txtdataの1行目「水素1」のデータを表示
@@ -105,13 +112,13 @@ for Ye0 in vYe0_0:
                 txtdata_freezeout = "freezeout_" + filename + ".txt"
                 rjava.Replot()
                 screenshot = pyautogui.screenshot()
-                path_pic_freezeout = "../進捗/0916to0924/sample/pic/freezeout_"+ filename + ".png"
+                path_pic_freezeout = "../進捗/1004to1011/pic/freezeout_"+ filename + ".png"
                 screenshot.save(path_pic_freezeout)
                 rjava.Data()
                 rjava.DtSv(txtdata_freezeout)
 
                 # freezeout時のデータを表示
-                path_open_freezeout = "../進捗/0916to0924/sample/data/" + txtdata_freezeout
+                path_open_freezeout = "../進捗/1004to1011/data/" + txtdata_freezeout
                 with open(path_open_freezeout) as f:
                     line = f.readlines()
                 print(line[1])                
@@ -130,12 +137,13 @@ for Ye0 in vYe0_0:
                     
                     cnt2 = 0
                     jouken = True
-                    while (jouken):
+                    while (jouken and cnt2 <= 100):
                         # 0.何回目、保存名、保存場所
                         now = datetime.datetime.now()
-                        print("time : %d, %d 回目" % time,cnt2)
+                        print(now)
+                        print("%d 回目" % cnt2)
                         txtdata_information = "InformationData_" + filename + "_cnt_" + str(cnt2) + ".txt"
-                        path_information = "../進捗/0916to0924/sample/data/" + txtdata_information
+                        path_information = "../進捗/1004to1011/data/" + txtdata_information
                         
                         # 1.informationモジュールを開く
                         rjava.Information()
@@ -153,35 +161,48 @@ for Ye0 in vYe0_0:
                             line = f.readlines()  
                         
                         # 4.Done が表示されるであろう行数を読み込み、Done があるかどうか判別する。
-                        target = cnt_column - 4
-                        if target < 0:
-                            target = 0
-                        print(line[target])
+                        target1 = cnt_column - 1
+                        target2 = cnt_column - 2
+                        target3 = cnt_column - 3
+                        target4 = cnt_column - 4
+                        
+                        if target1 < 0:
+                            target1 = 0
+                        print(line[target1])
+                        if target2 < 0:
+                            target2 = 0
+                        print(line[target2])
+                        if target3 < 0:
+                            target3 = 0
+                        print(line[target3])
+                        if target4 < 0:
+                            target4 = 0
+                        print(line[target4])
 
                         # 5.Done があれば計算終了
                             # 読み込みデータ数が7でなければDoneはない
-                        a = line[target].split()
-                        cnt10 = 0
-                        for i in a:
-                            cnt10 += 1
-                            #読み込みデータ数が7であり、要素７が"DONE"なら終了
-                        if cnt10 == 7:
-                            hoge1, hoge2, hoge3, hoge4, hoge5, hoge6, hoge7 = a
-                            print(hoge7)
-                            if hoge7 == "DONE":
-                                jouken = False
-                            else:
-                                cnt2 += 1
-                                time.sleep(600)
+                        a1 = line[target1]
+                        a2 = line[target2]
+                        a3 = line[target3]
+                        a4 = line[target4]
                         
+                        result1 = 'DONE' in a1
+                        result2 = 'DONE' in a2
+                        result3 = 'DONE' in a3
+                        result4 = 'DONE' in a4
+
+                        if result1 or result2 or result3 or result4:
+                            jouken = False
+                            
                         # 6.Done がなければ10分待って3.~4.以降を再度実行
                         else:
                             cnt2 += 1
                             time.sleep(600)
 
+
                     rjava.graph2()
                     rjava.Replot()
-                    path_last = "../進捗/0916to0924/sample/pic/last_" + filename + ".png"
+                    path_last = "../進捗/1004to1011/pic/last_" + filename + ".png"
                     screenshot2 = pyautogui.screenshot()
                     screenshot2.save(path_last)
                     rjava.Data()# dataを開く
